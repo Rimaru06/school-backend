266ce4f9c060e2746c8ab5fa57a91b9b86477d47
rimaru06 <shivraodanger04@gmail.com>
2025-05-10 02:20:03 +0530

    fixd index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const galleryRoutes = require('./routes/galleryRoutes');
 const documentRoutes = require('./routes/documentRoutes');
