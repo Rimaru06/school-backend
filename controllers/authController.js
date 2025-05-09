bda1669da91cafa5e20f417e01529fc7455817a4
rimaru06 <shivraodanger04@gmail.com>
2025-05-10 00:45:03 +0530

    addtional update

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,5 +16,5 @@ exports.login = async (req, res) => {
     return res.status(400).json({ message: 'Invalid credentials' });
   }
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.json({ token });
+  res.json({ token , role : user.role });
 };
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const galleryRoutes = require('./routes/galleryRoutes');
 const documentRoutes = require('./routes/documentRoutes');
 const newsRoutes = require('./routes/newsRoutes');
 const app = express();
 
+app.use(cors());
 dotenv.config();
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
