944a571deebec4eaafedb15f06546a0b35504b9a
rimaru06 <shivraodanger04@gmail.com>
2025-05-10 02:23:29 +0530

    make static

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const galleryRoutes = require('./routes/galleryRoutes');
 const documentRoutes = require('./routes/documentRoutes');
@@ -12,7 +11,8 @@ const app = express();
 app.use(cors());
 dotenv.config();
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
   res.json({message : "running good"})
